77e902625678397bc7fd545f8414cf30d6c9a4cc
Peter Fedrizzi <peterfedrizzi@Peters-MacBook.local>
2018-01-21 14:41:43 -0500

    print bizz on multiples of 7

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,11 +2,19 @@
 
 def fizzbuzz(n):
     for i in range(1, n + 1):
-        if i % 15 == 0:
+        if i % 105 == 0:
+	    print "fizzbuzzbizz"
+	elif i % 35 == 0:
+	    print "buzzbizz"
+	elif i % 21 == 0:
+	    print "fizzbizz"
+	if i % 15 == 0:
             print "fizzbuzz"
         elif i % 3 == 0:
             print "fizz"
         elif i % 5 == 0:
             print "buzz"
+	elif i % 7 == 0:
+	    print "bizz"
         else:
             print i
